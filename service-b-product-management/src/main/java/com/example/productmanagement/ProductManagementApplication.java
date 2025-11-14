93a92f1bb4af13d6e56c426b00706d7e436e0dd5
arevelo <arevelo@pichincha.com>
2025-11-14 11:41:31 -0500

    feat: add product service structure

--- service-b-product-management/src/main/java/com/example/productmanagement/ProductManagementApplication.java
+++ service-b-product-management/src/main/java/com/example/productmanagement/ProductManagementApplication.java
@@ -1,5 +1,18 @@
-public class ProductManagementApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ProductManagementApplication.class, args);
+package com.example.productmanagement; 
+
+ 
+
+import org.springframework.boot.SpringApplication; 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication 
+
+public class ProductManagementApplication { 
+
+    public static void main(String[] args) {  s  
+
+        SpringApplication.run(ProductManagementApplication.class, args); 
+
     }
-}
+} 
